1bbcf0346302bdafeab3f098ec64206f63517c83
kushagraverma26 <kushagraverma26@gmail.com>
2020-07-24 22:59:38 +0530

    complete

--- routes/buyers.js
+++ routes/buyers.js
@@ -37,72 +37,59 @@ router.get("/myProfile", buyerValidate, (req, res) => {
 })
 
 
-
-//Pay Bill
-//Use after recording payment
-// router.post("/payBill", buyerValidate, (req, res) => {
-//     tokenToId(req.get("token")).then((id) => {
-//         buyers.findByIdAndUpdate(id, { $inc: { 'outstandingBill': req.body.amount * -1 } }, { new: true }, function (err, buyer) {
-//             if (err) {
-//                 res.status(500).send("DB error")
-//             }
-//             else {
-//                 res.send([{ "outstandingBill": buyer.outstandingBill }])
-//             }
-//         })
-//     }).catch((err) => {
-//         res.status(400).send("Bad Request")
-//     })
-// })
-
-
-
 // API to create order for Razorpay
 router.get("/order", buyerValidate, (req, res) => {
+    console.log("starting order");
     tokenToId(req.get("token")).then((id) => {
         buyers.find({ "_id": id }).then((buyer) => {
-            var amount = buyer['outstandingBill'];
-            try {
-                const options = {
-                    amount: amount * 100, // amount == Rs 10
-                    currency: "INR",
-                    receipt: "receipt " + amount.toString(),
-                    payment_capture: 0,
-                }; instance.orders.create(options, async function (err, order) {
-                    if (err) {
-                        return res.status(500).json({
-                            message: "Something Went Wrong",
-                        });
-                    }
-                    return res.status(200).json(order);
-                });
-            } catch (err) {
-                return res.status(500).json({
-                    message: "Something Went Wrong",
-                });
-            }
+            // console.log(buyer);
+            console.log(buyer[0].outstandingBill);
+            console.log(buyer[0]['outstandingBill']);
+
+            var amt = (buyer[0].outstandingBill * 100) + 100;
+            const options = {
+                amount: amt, // amount == Rs 10
+                currency: "INR",
+                receipt: "receipt " + amt.toString(),
+                payment_capture: 0,
+            };
+            console.log(options);
+            instance.orders.create(options, async function (err, order) {
+                if (err) {
+                    return res.status(500).json({
+                        message: "Something Went Wrong while creating order",
+                    });
+                }
+                return res.status(200).json(order);
+            });
         }).catch((dberror) => {
-            res.status(400).send("Something went wrong")
+            console.log(dberror);
+            res.status(400).send("Something went wrong in finding buyer")
         })
     }).catch((err) => {
+        console.log(err);
         res.status(400).send("Bad Request")
     })
 });
 
 
+
 // Razorpay payment 
-app.post("/capture/:paymentId", buyerValidate, (req, res) => {
+// Completes payment and settles the bill
+router.post("/capture/:paymentId", buyerValidate, (req, res) => {
+    console.log("starting payment");
     tokenToId(req.get("token")).then((id) => {
         try {
             return request({
                 method: "POST",
                 url: `https://rzp_test_e60Ree2kIvf22h:dzCakpcWRcFtgff0XXh087Pe@api.razorpay.com/v1/payments/${req.params.paymentId}/capture`,
                 form: {
-                    amount: 10 * 100, // amount == Rs 10 // Same As Order amount
+                    amount: req.body.amount,
                     currency: "INR",
                 },
             }, async function (err, response, body) {
                 if (err) {
+                    console.log(err);
                     return res.status(500).json({
                         message: "Something Went Wrong",
                     });
@@ -110,6 +97,8 @@ app.post("/capture/:paymentId", buyerValidate, (req, res) => {
                 else {
                     buyers.findByIdAndUpdate(id, { $inc: { 'outstandingBill': 0 } }, { new: true }, function (err, buyer) {
                         if (err) {
+                            console.log(err);
+
                             res.status(500).send("DB error while settleing bill")
                         }
                         else {
@@ -122,6 +111,7 @@ app.post("/capture/:paymentId", buyerValidate, (req, res) => {
                 }
             });
         } catch (err) {
+            console.log(err);
             return res.status(500).json({
                 message: "Something Went Wrong",
             });
